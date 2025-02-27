1c4a99736dc8ce8e2e978d0358d3fced444db2f6
Khan <49835781+latency113@users.noreply.github.com>
2025-02-27 23:16:29 +0700

    Update booking.service.js

--- src/services/booking.service.js
+++ src/services/booking.service.js
@@ -33,7 +33,7 @@ const getConcertById = async (id) => {
 const createBooking = async (bookingData, token) => {
   try {
     const response = await http.post(
-      "https://concert-production.up.railway.app", // Your backend API endpoint
+      "https://concert-production.up.railway.app/", // Your backend API endpoint
       bookingData, // Send booking data in the body
       {
         headers: {
