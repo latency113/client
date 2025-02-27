3686bba50b49da4b1e7d37d609bc61b153998d41
Khan <49835781+latency113@users.noreply.github.com>
2025-02-27 21:16:54 +0700

    Update http-common.js

--- http-common.js
+++ http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://concert-yyqf.onrender.com/",
+  baseURL: "concert-production.up.railway.app/",
   headers: {
     "Content-type": "application/json"
   }
