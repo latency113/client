cda69a4fefb92c8f510013ca26f8f89bfba7b212
Khan <49835781+latency113@users.noreply.github.com>
2025-05-21 17:55:32 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     const token = localStorage.getItem("token");
 
     if (token) {
-      fetch("https://concert-production.up.railway.app/api/user/profile", {
+      fetch("concert-yyqf.onrender.com/api/user/profile", {
         method: "GET",
         headers: {
           Authorization: `Bearer ${token}`,
